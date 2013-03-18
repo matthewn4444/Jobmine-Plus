21796de260c897dda06f624ea43850f273015c81
Matthew Ng <matthewn4444@gmail.com>
2013-03-17 21:36:08 -0400

    Auto login now depends on the settings

--- mobile/android/src/com/jobmineplus/mobile/activities/LoginActivity.java
+++ mobile/android/src/com/jobmineplus/mobile/activities/LoginActivity.java
@@ -3,7 +3,9 @@ package com.jobmineplus.mobile.activities;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Pair;
@@ -36,11 +38,17 @@ public class LoginActivity extends SimpleActivityBase implements OnClickListener
         userDataSource = new UserDataSource(this);
         userDataSource.open();
 
-        // Check for login credentials
-        // If this fails on startup, make a launcher activity instead to read credentials on thread
-        Pair<String, String> credentials = userDataSource.getLastUser();
-        if (credentials != null) {
-            goToHomeActivityAndLogin(credentials.first, credentials.second);
+        // Check preferences to see if we should autologin
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (sharedPrefs.getBoolean("settingsAutoLogin", false)) {
+            // Check for login credentials
+            // If this fails on startup, make a launcher activity instead to read credentials on thread
+            Pair<String, String> credentials = userDataSource.getLastUser();
+            if (credentials != null) {
+                goToHomeActivityAndLogin(credentials.first, credentials.second);
+            } else {
+                defindUiAndAttachEvents();
+            }
         } else {
             defindUiAndAttachEvents();
         }
