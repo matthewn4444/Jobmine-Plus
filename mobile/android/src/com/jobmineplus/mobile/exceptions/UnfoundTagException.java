125c046dfe80e92a79b3c15764a9d1c96e474489
Matthew Ng <matthewn4444@gmail.com>
2013-06-12 22:49:28 -0700

    Updated parsing error message

---
+++ mobile/android/src/com/jobmineplus/mobile/exceptions/UnfoundTagException.java
@@ -0,0 +1,9 @@
+package com.jobmineplus.mobile.exceptions;
+
+    public class UnfoundTagException extends JbmnplsTableException {
+        private static final long serialVersionUID = 6844014422939345882L;
+        public UnfoundTagException() {}
+        public UnfoundTagException(String message) {
+            super(message);
+        }
+    }
