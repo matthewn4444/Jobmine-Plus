44dbb96c7762daf24edb2532a3758a00efc11c21
Matthew Ng <matthewn4444@gmail.com>
2013-07-25 23:53:34 -0700

    Added a customizable interview test page

---
+++ test-sever/data/interviews-test.js
@@ -0,0 +1,8 @@
+exports.data = [
+    ["00207062", "AMD", "Driver Developer", "13 JAN 2012", "In Person", "NOPE", "10:00 am", 45, "Room 13", "There is instructions", "Matthew Ng", "Scheduled"],
+    ["00207135", "AMD", "Co-op Engineering - Validation", "22 MAY 2013", "Video", "NOPE", "11:00 am", 45, "Room 13", "There is instructions", "Matthew Ng", "Screened"],
+    ["00206277", "Altera Corportation", "Software Engineering", "21 MAY 2013", "Phone", "NOPE", "11:00 am", 45, "Room 15", "There is instructions", "Matthew Ng", "Screened"],
+    ["00204599", "Apple Inc", "Silicon Engineering Hardware", "20 MAY 2013", "Phone", "NOPE", "12:00 am", 30, "Room 15", "There is instructions", "Matthew Ng", "Scheduled"],
+    ["00205758", "Avvasi Inc", "Software Design Engineering Embedded (C++)", "20 MAY 2013", "Phone", "NOPE", "1:00 pm", 30, "Room 15", "There is instructions", "Matthew Ng", "Scheduled"],
+    ["00207571", "COM DEV International Ltd", "Fabrication Analyst / Software Developer", "20 MAY 2013", "Phone", "NOPE", "1:00 pm", 30, "Room 15", "There is instructions", "Matthew Ng", "Ranking Completed"],
+];
